7f0483542d7295dd3c4091a4d207f2e2734ae4e9
srira-amirishetty <157988308+srira-amirishetty@users.noreply.github.com>
2025-01-11 15:30:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://sriram:camps@cluster0.88kqnqd.mongodb.net/todolist",{ useNewUrlParser: true,useUnifiedTopology: true } );
+mongoose.connect("",{ useNewUrlParser: true,useUnifiedTopology: true } );
 
 const itemsSchema = {
   name: String
